43048fe99f168fbf0d6087d176234a6b8bf01de7
hugo meiland <hugo.meiland@microsoft.com>
2023-12-18 08:43:06 +0100

    clean up code with flake8

--- amlslurm/__init__.py
+++ amlslurm/__init__.py
@@ -1,25 +1,25 @@
 def sinfo(vargs):
-    import os,sys
+    import os
     from azureml.core import Workspace
     from azureml.core.authentication import AzureCliAuthentication
     from azureml.core.compute import ComputeTarget, AmlCompute
 
-    subscription_id=os.environ['SUBSCRIPTION']
-    resource_group=os.environ['CI_RESOURCE_GROUP']
-    workspace_name=os.environ['CI_WORKSPACE']
-    pwd=os.environ['PWD']
+    subscription_id = os.environ['SUBSCRIPTION']
+    resource_group = os.environ['CI_RESOURCE_GROUP']
+    workspace_name = os.environ['CI_WORKSPACE']
+    # pwd=os.environ['PWD']
 
-    from azure.ai.ml import MLClient
-    from azure.identity import DefaultAzureCredential
+    # from azure.ai.ml import MLClient
+    # from azure.identity import DefaultAzureCredential
 
-    credential = DefaultAzureCredential()
+    # credential = DefaultAzureCredential()
 
-    ml_client = MLClient(
-        credential=credential,
-        subscription_id=subscription_id,
-        resource_group_name=resource_group,
-        workspace_name=workspace_name,
-        )
+    # ml_client = MLClient(
+    #    credential=credential,
+    #    subscription_id=subscription_id,
+    #    resource_group_name=resource_group,
+    #    workspace_name=workspace_name,
+    #    )
 
     cli_auth = AzureCliAuthentication()
     ws = Workspace(
@@ -33,23 +33,25 @@ def sinfo(vargs):
     print("PARTITION\tAVAIL\tVM_SIZE\t\t\tNODES\tSTATE")
     for i in sinfo_list:
         line = (AmlCompute.get(i).get('name'))
-        if len(line) < 8: line += "\t"
+        if len(line) < 8:
+            line += "\t"
         line += "\tUP\t" + AmlCompute.get_status(i).vm_size + "\t"
-        if len(line.expandtabs()) < 41 : line += "\t"
-        line += str(AmlCompute.get(i).get('properties',{}).get('properties',{}).get('scaleSettings',{}).get('maxNodeCount'))
+        if len(line.expandtabs()) < 41:
+            line += "\t"
+        line += str(AmlCompute.get(i).get('properties', {}).get('properties', {}).get('scaleSettings', {}).get('maxNodeCount'))
         print(line)
 
 
 def squeue(vargs):
-    import os,sys
-    from azureml.core import Workspace
-    from azureml.core.authentication import AzureCliAuthentication
-    from azureml.core.compute import ComputeTarget, AmlCompute
+    import os
+    # from azureml.core import Workspace
+    # from azureml.core.authentication import AzureCliAuthentication
+    # from azureml.core.compute import ComputeTarget, AmlCompute
 
-    subscription_id=os.environ['SUBSCRIPTION']
-    resource_group=os.environ['CI_RESOURCE_GROUP']
-    workspace_name=os.environ['CI_WORKSPACE']
-    pwd=os.environ['PWD']
+    subscription_id = os.environ['SUBSCRIPTION']
+    resource_group = os.environ['CI_RESOURCE_GROUP']
+    workspace_name = os.environ['CI_WORKSPACE']
+    # pwd=os.environ['PWD']
 
     from azure.ai.ml import MLClient
     from azure.identity import DefaultAzureCredential
@@ -63,13 +65,13 @@ def squeue(vargs):
         workspace_name=workspace_name,
         )
 
-    cli_auth = AzureCliAuthentication()
-    ws = Workspace(
-        subscription_id=os.environ['SUBSCRIPTION'],
-        resource_group=os.environ['CI_RESOURCE_GROUP'],
-        workspace_name=os.environ['CI_WORKSPACE'],
-        auth=cli_auth
-        )
+    # cli_auth = AzureCliAuthentication()
+    # ws = Workspace(
+    #    subscription_id=subscription_id,
+    #    resource_group=resource_group,
+    #    workspace_name=workspace_name,
+    #    auth=cli_auth
+    #    )
 
     job_list = []
     list_of_jobs = ml_client.jobs.list()
@@ -78,24 +80,25 @@ def squeue(vargs):
     print("JOBID\t\t\t\tNAME\t\tPARTITION\tSTATE\tTIME")
     for job in job_list:
         line = job.name
-        if len(line) < 24: line += "\t"
+        if len(line) < 24:
+            line += "\t"
         line += "\t" + job.display_name
-        if len(line) < 8: line += "\t"
+        if len(line) < 8:
+            line += "\t"
         line += "\t" + str(job.compute)
-        #line += "\t" + str(job.properties.EndTimeUtc)
-        #line += "\t" + str(job.creation_context.created_by)
         print(line)
 
-def sbatch(vargs):
-    import os,sys
-    from azureml.core import Workspace
-    from azureml.core.authentication import AzureCliAuthentication
-    from azureml.core.compute import ComputeTarget, AmlCompute
 
-    subscription_id=os.environ['SUBSCRIPTION']
-    resource_group=os.environ['CI_RESOURCE_GROUP']
-    workspace_name=os.environ['CI_WORKSPACE']
-    pwd=os.environ['PWD']
+def sbatch(vargs):
+    import os
+    # from azureml.core import Workspace
+    # from azureml.core.authentication import AzureCliAuthentication
+    # from azureml.core.compute import ComputeTarget, AmlCompute
+
+    subscription_id = os.environ['SUBSCRIPTION']
+    resource_group = os.environ['CI_RESOURCE_GROUP']
+    workspace_name = os.environ['CI_WORKSPACE']
+    pwd = os.environ['PWD']
 
     from azure.ai.ml import MLClient
     from azure.identity import DefaultAzureCredential
@@ -109,32 +112,25 @@ def sbatch(vargs):
         workspace_name=workspace_name,
         )
 
-    cli_auth = AzureCliAuthentication()
-    ws = Workspace(
-        subscription_id=subscription_id,
-        resource_group=resource_group,
-        workspace_name=workspace_name,
-        auth=cli_auth
-        )
+    # cli_auth = AzureCliAuthentication()
+    # ws = Workspace(
+    #    subscription_id=subscription_id,
+    #    resource_group=resource_group,
+    #    workspace_name=workspace_name,
+    #    auth=cli_auth
+    #    )
 
     from azure.ai.ml import command
     import argparse
 
     parser = argparse.ArgumentParser(description='sbatch: submit jobs to Azure Machine Learning')
-    parser.add_argument('-a','--array', default="None", type=str, 
-            help='index for array jobs')
-    parser.add_argument('-p','--partition', type=str, required=True,
-            help='set compute partition where the job should be run. Use <sinfo> to view available partitions')
-    parser.add_argument('-N','--nodes', default=1, type=int, 
-            help='amount of nodes to use for the job')
-    parser.add_argument('-w','--wrap', type=str, 
-            help='command line to be executed, should be enclosed with quotes')
-    parser.add_argument('script', nargs='?', default="None", type=str, 
-            help='script to be executed')
-    #args = parser.parse_args()
+    parser.add_argument('-a', '--array', default="None", type=str, help='index for array jobs')
+    parser.add_argument('-p', '--partition', type=str, required=True,
+                        help='set compute partition where the job should be run. Use <sinfo> to view available partitions')
+    parser.add_argument('-N', '--nodes', default=1, type=int, help='amount of nodes to use for the job')
+    parser.add_argument('-w', '--wrap', type=str, help='command line to be executed, should be enclosed with quotes')
+    parser.add_argument('script', nargs='?', default="None", type=str, help='script to be executed')
     args = parser.parse_args(vargs)
-    #print("script is " + args.script)
-    #print("nodes is " + str(args.nodes))
 
     if (args.script == "None") and (args.wrap is None):
         print("Missing: provide either script to execute as argument or commandline to execute through --wrap option")
@@ -146,22 +142,21 @@ def sbatch(vargs):
 
     if (args.script != "None"):
         command_job = command(
-            code=pwd + "/" + args.script,#code="/home/azureuser/cloudfiles/code/Users/gaobrien/GeophysicsAML/gsobrien_F_shot_generate.py",
-            command=args.script, #command="hostname",
-            environment= "docker-test1:4",
+            code=pwd + "/" + args.script,
+            command=args.script,
+            environment="docker-test1:4",
             instance_count=args.nodes,
-            compute=args.partition, #compute="hc44",
-            #display_name="testjob"
+            compute=args.partition,
+            # display_name="testjob"
             )
 
     if (args.wrap is not None):
         command_job = command(
-            #code="/home/azureuser/cloudfiles/code/Users/gaobrien/GeophysicsAML/gsobrien_F_shot_generate.py",
-            command=args.wrap, #command="hostname",
-            environment= "docker-test1:4",
+            command=args.wrap,
+            environment="docker-test1:4",
             instance_count=args.nodes,
-            compute=args.partition, #compute="hc44",
-            #display_name="testjob"
+            compute=args.partition,
+            # display_name="testjob"
             )
 
     returned_job = ml_client.jobs.create_or_update(command_job)
