c5ee1cebf28a6bf55117725b08ffebf81b864c12
hugo meiland <hugo.meiland@microsoft.com>
2024-07-10 17:25:39 +0200

    stay on 2.3.2 version

--- deploy/aks-eessi/mirror_containers_and_install_csi.sh
+++ deploy/aks-eessi/mirror_containers_and_install_csi.sh
@@ -17,13 +17,13 @@ docker tag registry.k8s.io/sig-storage/csi-provisioner:v3.5.0 ${1}.azurecr.io/si
 docker push ${1}.azurecr.io/sig-storage/csi-provisioner:v3.5.0
 
 
-git clone -b release-2.4 https://github.com/cvmfs-contrib/cvmfs-csi.git
+git clone -b release-2.3 https://github.com/cvmfs-contrib/cvmfs-csi.git
 pushd cvmfs-csi/deployments/helm/cvmfs-csi
 sed -i "s#registry.cern.ch#${1}.azurecr.io#g" values.yaml
 sed -i "s#registry.k8s.io#${1}.azurecr.io#g" values.yaml
 sed -i 's#CVMFS_HTTP_PROXY=.*$#CVMFS_HTTP_PROXY=DIRECT#g' values.yaml
 helm package .
-helm push cvmfs-csi-2.1.1.tgz oci://${1}.azurecr.io/helm
+helm push cvmfs-csi-2.3.2.tgz oci://${1}.azurecr.io/helm
 popd
 
 helm install cvmfs oci://${1}.azurecr.io/helm/cvmfs-csi
